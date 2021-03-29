c0d714f2fb5e7757da91d9b0033f460481d1ff08
Gabriele Benasso <gabriele.benasso@gmail.com>
2021-03-29 10:07:11 +0200

    fine tour of heroes

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -10,6 +10,12 @@ import { catchError, map, tap } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class HeroService {
+  // deleteHero(id: number) {
+  //   throw new Error("Method not implemented.");
+  // }
+  // addHero(arg0: Hero) {
+  //   throw new Error("Method not implemented.");
+  // }
   private heroesUrl = 'api/heroes'; // URL to web api
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -68,6 +74,22 @@ updateHero(hero: Hero | undefined): Observable<any> {
   return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
     tap(_ => this.log(`updated hero id=${hero?.id}`)),
     catchError(this.handleError<any>('updateHero'))
+  );
+}
+/** POST: add a new hero to the server */
+addHero(hero: Hero): Observable<Hero> {
+  return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+    tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+    catchError(this.handleError<Hero>('addHero'))
+  );
+}
+/** DELETE: delete the hero from the server */
+deleteHero(id: number): Observable<Hero> {
+  const url = `${this.heroesUrl}/${id}`;
+
+  return this.http.delete<Hero>(url, this.httpOptions).pipe(
+    tap(_ => this.log(`deleted hero id=${id}`)),
+    catchError(this.handleError<Hero>('deleteHero'))
   );
 }
 }
